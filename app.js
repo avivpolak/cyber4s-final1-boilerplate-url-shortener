5b374d27a98b70e4d498daacde138f84ee0cabac
[aviv polak] <[avivp0437@gmail.com]>
2021-11-02 13:53:41 +0200

    redirect to google

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ app.use(cors());
 
 app.use("/public", express.static(`./public`));
 
+app.get("/api", (req, res) => {
+    res.setHeader(
+        "location",
+        "https://stackoverflow.com/questions/40840852/difference-between-res-setheader-and-res-header-in-node-js"
+    );
+
+    res.status(301);
+    res.send("redirect?");
+});
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
