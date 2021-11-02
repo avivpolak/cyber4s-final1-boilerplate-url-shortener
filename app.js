62e307ce77f4b6d75f40d3d3ee02bce4f52cb727
[aviv polak] <[avivp0437@gmail.com]>
2021-11-02 15:53:01 +0200

    a liitle stuck

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const { json } = require("body-parser");
 const app = express();
 
 app.use(cors());
@@ -10,13 +13,19 @@ app.use("/public", express.static(`./public`));
 app.get("/api/:id", (req, res) => {
     res.setHeader("location", getUrl(req.params.id));
     res.status(301);
-    res.send("redirect?");
+    res.send("redirect");
 });
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
+app.post("/api/shorturl/new", (req, res) => {
+    //res.send("url");
+
+    res.sendFile(__dirname + "/views/new.html");
+});
 
 module.exports = app;
 function getUrl(id) {
-    return `https://www.${id}.com`;
+    let db = fs.readFileSync(path.resolve(__dirname, "./db/dataBase.json"));
+    return JSON.parse(db)[id];
 }
---
+++ views/front.js
@@ -0,0 +1,5 @@
+document.getElementById("post").addEventListener("click", handleSubmit);
+function handleSubmit(event) {
+    let url = document.getElementById("url_input").value;
+    console.log(url);
+}
